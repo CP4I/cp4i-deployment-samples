8ac99f65889afe6ba889e4fb5c5a0c7e1f06b79f
Drew Douglass <drewd@us.ibm.com>
2024-04-19 10:51:58 -0400

    Update deploy-ia.sh
    Updated default storage classes

--- DrivewayDentDeletion/Operators/deploy-ia.sh
+++ DrivewayDentDeletion/Operators/deploy-ia.sh
@@ -24,8 +24,8 @@ CURRENT_DIR=$(dirname $0)
 source $CURRENT_DIR/../../products/bash/utils.sh
 
 NAMESPACE="cp4i"
-BLOCK_STORAGE_CLASS="cp4i-block-performance"
-FILE_STORAGE_CLASS="cp4i-file-performance-gid"
+BLOCK_STORAGE_CLASS="ocs-storagecluster-ceph-rbd"
+FILE_STORAGE_CLASS="ocs-storagecluster-cephfs"
 DDD_ENV="dev"
 APIC="false"
 
